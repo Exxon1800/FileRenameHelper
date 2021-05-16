97df1251d3ff22de3c1bdb8e576bd40095d31ca1
Arnout <Exxon1800@users.noreply.github.com>
2021-05-16 23:17:21 +0200

    Implemented folder selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,12 @@
-const actualBtn = document.getElementById('actual-btn');
+const chooseFileButton = $('#choose-file-button');
+const fileChosen = $('#file-chosen');
 
-const fileChosen = document.getElementById('file-chosen');
-
-actualBtn.addEventListener('change', function(){
-    fileChosen.textContent = this.files.length + (this.files.length > 1 ? " files chosen" : " file chosen")
-    let files = [];
-    for (let i = 0; i < this.files.length; i++) {
-        let file = this.files[i]
-        files.push({
-            'lastModified': file.lastModified,
-            'lastModifiedDate': file.lastModifiedDate,
-            'name': file.name,
-            'size': file.size,
-            'type': file.type
-        })
-    }
-    $.post('/submit-files', JSON.stringify(files))
-})
+chooseFileButton.on('click', function(){
+    let files = $.get('/choose-files', function(data){
+        console.log(data)
+    }).fail(function() {
+        console.error("could not get files")
+    })
+    // fileChosen.textContent = this.files.length + (this.files.length > 1 ? " files chosen" : " file chosen")
+});
 
